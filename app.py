026715bfd5db76b40684f3a51850d5aba3c0d528
SerMeliodas <oliver9574@gmail.com>
2022-08-07 18:19:37 +0200

    delete import.py file

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from imports import *
+import sys
+import os
 
+from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PySide6.QtCore import QTimer
+from PySide6.QtGui import QAction
+
+from ui import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- imports.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog
-from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
-from PySide6.QtCore import QTimer
-from PySide6.QtGui import QAction
-
-from ui import MainWindow
